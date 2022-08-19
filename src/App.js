83873022883ce8e59a02dff1ce4a7153c6f89dd4
leandro-miranda <leandromiranda2010@gmail.com>
2022-08-19 18:33:12 -0300

    requisitos 5 e 6 feitos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './components/Login';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Pages/Login';
+import Comidas from './Pages/Comidas';
 
 function App() {
   return (
     <div className="meals">
-      <Login />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/foods" component={ Comidas } />
+          <Route exact path="/" component={ Login } />
+
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Comidas.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Comidas extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Comidas</h1>
+      </div>
+    );
+  }
+}
