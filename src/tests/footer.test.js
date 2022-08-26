90db1c67cc6cdb51e572489867510b292a42d6f8
leandro-miranda <leandromiranda2010@gmail.com>
2022-08-26 19:35:07 -0300

    refarotando testes

--- src/tests/footer.test.js
+++ src/tests/footer.test.js
@@ -1,12 +1,14 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import Foods from '../Pages/Foods';
 import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
 
 describe('Tests the Footer component', () => {
   it('Tests if components render correctly',()=> {
-    renderWithRouter(<Foods />)
+    const {history} = renderWithRouter(<App />)
+    history.push('/foods')
+
     const btnDrinks = screen.getByTestId('drinks-bottom-btn')
     const btnFood = screen.getByTestId('food-bottom-btn');
 
@@ -15,19 +17,26 @@ describe('Tests the Footer component', () => {
   });
 
   it('Tests if Footer can be redirect to foods or drinks', ()=> {
-    const {history} = renderWithRouter(<Foods />)
+    const {history} = renderWithRouter(<App />)
+    history.push('/foods')
 
-    const btnDrinks = screen.getByTestId('drinks-bottom-btn')
-    const btnFood = screen.getByTestId('food-bottom-btn');
+    const btnDrinks = screen.getByTestId('drinks-bottom-btn');
 
     userEvent.click(btnDrinks);
     expect(history.location.pathname).toBe('/drinks');
+    const imgDrinks = screen.getByRole('img', { name: /drink/i });
+    expect(imgDrinks).toHaveAttribute('src', 'drinkIcon.svg');
+  });
+
+  it('Tests if Footer can be redirect to foods or drinks', ()=> {
+    const {history} = renderWithRouter(<App />)
+    history.push('/foods')
+
+    const btnFood = screen.getByTestId('food-bottom-btn');
+
     userEvent.click(btnFood);
     expect(history.location.pathname).toBe('/foods');
     const imgDrinks = screen.getByRole('img', { name: /drink/i });
     expect(imgDrinks).toHaveAttribute('src', 'drinkIcon.svg');
-    /* const imgFoods = screen.getByRole('img', { name: /meal/i });
-    expect(imgFoods).toHaveAttribute('src', 'mealIcon.svg'); */
   });
-
 });
